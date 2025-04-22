011e24ea44d23c3c69e407c7675e16bce9b60e42
Yuhan Park <156402683+pbk2312@users.noreply.github.com>
2025-04-23 00:40:55 +0900

    Create Main.java
    dasd

---
+++ Main.java
@@ -0,0 +1,10 @@
+@SpringBootApplication
+@EnableCaching
+@ConfigurationPropertiesScan
+public class CafeFinderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CafeFinderApplication.class, args);
+    }
+
+}
